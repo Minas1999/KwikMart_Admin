a6a6d9f681d0785c8b1c023635f5cf3461ac4e69
Մինաս Սանդաձե <minas.sandadze@armsoft.am>
2022-03-10 17:54:51 +0400

    Add Some functions in Login page

--- app/src/main/java/com/example/kwikmart_admin/LoginActivity.java
+++ app/src/main/java/com/example/kwikmart_admin/LoginActivity.java
@@ -1,14 +1,54 @@
 package com.example.kwikmart_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton fb, google, twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        fb = findViewById(R.id.fab_facebook);
+        google = findViewById(R.id.fab_facebook1);
+        twitter = findViewById(R.id.fab_facebook2);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("SignUp"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        fb.setTranslationY(300);
+        google.setTranslationY(300);
+        twitter.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+        fb.setAlpha(30);
+        google.setAlpha(30);
+        twitter.setAlpha(30);
+        tabLayout.setAlpha(1);
+
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
+
     }
 }
